b34441425275f6eb5c962fe62b8686119cd1ce99
DinhVanNam1103 <dinhvannam11032003@gmail.com>
2023-05-16 03:17:55 +0700

    update sql

--- database/Database_Web_2.sql
+++ database/Database_Web_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 14, 2023 lúc 04:11 PM
+-- Thời gian đã tạo: Th5 15, 2023 lúc 10:15 PM
 -- Phiên bản máy phục vụ: 10.4.27-MariaDB
 -- Phiên bản PHP: 7.4.33
 
@@ -87,19 +87,19 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`id`, `product_id`, `thumbnail`) VALUES
-(64, 55, '1683358447_BBB.jpg'),
-(65, 57, ''),
-(85, 58, '1683375880_Ruousoju.jpg'),
-(86, 58, '1683380546_Seoul-drink03242-1.jpg'),
-(87, 58, '1683380606_'),
-(88, 58, '1683380797_NETAO.jpg'),
-(89, 58, '1683380846_'),
-(90, 58, '1683381106_Seoul-drink03242-1.jpg'),
-(91, 58, '1683381113_'),
-(92, 58, '1683381501_NETAO.jpg'),
-(93, 58, '1683381530_Seoul-drink03242-1.jpg'),
-(94, 58, '1683381541_NETAO.jpg'),
-(96, 60, '1683790154_AVTC.jpg');
+(97, 10001, '1684180416_BBB.jpg'),
+(98, 10002, '1684180516_AVTC.jpg'),
+(99, 10003, '1684180593_MKCB.jpg'),
+(100, 10004, '1684180664_LTHSL.jpg'),
+(101, 10005, '1684180726_NETAO.jpg'),
+(102, 10006, '1684180800_TBHS.jpg'),
+(103, 10007, '1684180859_MKCBM.jpg'),
+(104, 10008, '1684180906_MKCC.jpg'),
+(105, 10009, '1684181047_BCXtm.jpg'),
+(106, 10010, '1684181112_MKCTCAM.jpg'),
+(107, 10011, '1684181182_TOMTM.jpg'),
+(108, 10012, '1684181228_MLTB.jpg'),
+(109, 10013, '1684181307_MLTG.jpg');
 
 -- --------------------------------------------------------
 
@@ -125,8 +125,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `fullname`, `phone`, `address`, `note`, `order_date`, `status`, `total_money`, `payment_methods`) VALUES
-(1, 5, 'le van hoan', '0354986796', 'dwadawada, Phường Phúc Xá, Quận Ba Đình, Thành phố Hà Nội', '', '2023-05-14 17:54:59', 1, 1153000, 'visa'),
-(2, 5, 'Hà Nguyễn', '0354986796', 'duawda, Thị trấn Pác Miầu, Huyện Bảo Lâm, Tỉnh Cao Bằng', '', '2023-05-14 17:58:23', 1, 1153000, 'visa');
+(4, 6, 'Lê Văn Hoàn', '0354986796', 'Nhị Bình 22, Phường Phúc Xá, Quận Ba Đình, Thành phố Hà Nội', '', '2023-05-16 03:14:19', 1, 320000, 'cash');
 
 -- --------------------------------------------------------
 
@@ -148,10 +147,8 @@ CREATE TABLE `orders_details` (
 --
 
 INSERT INTO `orders_details` (`id`, `order_id`, `product_id`, `price`, `num`, `total_money`) VALUES
-(1, 1, 58, 565000, 2, 1130000),
-(2, 1, 57, 23000, 1, 23000),
-(3, 2, 58, 565000, 2, 1130000),
-(4, 2, 57, 23000, 1, 23000);
+(8, 4, 10001, 55000, 2, 110000),
+(9, 4, 10004, 210000, 1, 210000);
 
 -- --------------------------------------------------------
 
@@ -169,18 +166,29 @@ CREATE TABLE `product` (
   `description` longtext NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `status` tinyint(11) DEFAULT NULL
+  `status` tinyint(11) DEFAULT NULL,
+  `sold` tinyint(4) NOT NULL DEFAULT 0,
+  `visible` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `product`
 --
 
-INSERT INTO `product` (`id`, `product_code`, `category_id`, `product_name`, `price`, `thumbnail`, `description`, `created_at`, `updated_at`, `status`) VALUES
-(55, 'MT101', 1002, 'A', 565005, '1683358447_BB.jpg', 'Đoạn code này lấy thông tin về file ảnh được chọn từ thẻ input có tên là img_file_small_update, lưu tên file vào biến $hinhanh và lưu đường dẫn tạm thời của file vào biến $hinhanh_tmp. Sau đó, nó sử dụng hàm time() để thêm một đoạn thời gian vào trước tên file để tránh bị trùng lặp tên file khi có nhiều người dùng upload cùng một tên file. Do đó, đoạn code này không xung đột với các đoạn code khác.Đoạn code này lấy thông tin về file ảnh được chọn từ thẻ input có tên là img_file_small_update, lưu tên file vào biến $hinhanh và lưu đường dẫn tạm thời của file vào biến $hinhanh_tmp. Sau đó, nó sử dụng hàm time() để thêm một đoạn thời gian vào trước tên file để tránh bị trùng lặp tên file khi có nhiều người dùng upload cùng một tên file. Do đó, đoạn code này không xung đột với các đoạn code khác.', '2023-05-06 09:34:07', NULL, 0),
-(57, 'MT1010', 1001, 'abn', 23000, '1683365975_BB.jpg', '', '2023-05-06 11:39:35', NULL, 1),
-(58, 'MT1010', 1004, 'daqq', 565000, '1683381541_Nuocgao.jpg', '', '2023-05-06 11:42:14', NULL, 1),
-(60, 'MT1010', 1007, 'Abc', 11111111, '1683790154_AVTC.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima odit, hic ab consectetur delectus vel natus ratione a ipsum, sit nam animi voluptate fugit sunt quam ea iure. Ipsa, dicta?\r\nTenetur ex eos, molestiae maiores iste delectus veniam laboriosam minus earum qui assumenda ea ullam minima. Hic natus, necessitatibus facere quam corporis, molestiae harum iure nostrum, vel corrupti quia est.\r\nConsequatur, debitis repellat sed officiis id quaerat quas labore vero maiores aut. Soluta, reiciendis quidem corrupti totam cupiditate sunt eveniet itaque sed quam molestiae, unde, nemo quod! Vitae, qui quasi?\r\nRem fugiat sint excepturi dolore perferendis? Veritatis quaerat ducimus, reiciendis rem nihil adipisci error, aperiam repellat reprehenderit quo consectetur quam consequuntur, ut accusantium labore ratione blanditiis eligendi optio tempora libero!\r\nAutem, totam eligendi reprehenderit non saepe, officiis sunt voluptatem mollitia quam aliquam sit ratione! Accusantium, asperiores! Perspiciatis consequuntur laudantium fugit tempore dicta ad facilis error temporibus. Voluptatum labore nam porro?\r\nModi similique nulla repudiandae totam alias, optio obcaecati a dolor, labore ipsum asperiores distinctio impedit accusamus consequatur mollitia eaque nemo placeat incidunt magni nam quia nisi, error animi ut. Inventore!\r\nDolor non, amet perferendis dolorem eum quia officia natus harum voluptatum aspernatur eius, molestiae quas debitis quae corrupti obcaecati optio maxime iste! Saepe sed consequuntur corporis ratione. Consequatur, qui eos?\r\nOdio, sunt. Voluptas voluptatem quos, cumque sunt accusamus natus expedita? Officiis dolor, quis suscipit qui debitis, laborum ipsa molestiae voluptas alias, quia possimus perspiciatis nostrum ad voluptate fugiat est mollitia?\r\nQuas nulla sint esse optio iste! Ut exercitationem cum facilis accusamus ipsum tenetur, est nulla iure ullam iusto obcaecati magnam dolor ipsa fugiat. Sunt excepturi cum quis officiis ipsa libero.\r\nOptio nam alias reprehenderit eaque nihil laudantium debitis, quos ab aperiam explicabo voluptates soluta? Voluptates delectus corporis quibusdam, reprehenderit dignissimos, necessitatibus quae aspernatur incidunt, libero impedit optio dolores odit! Nulla!\r\nIste necessitatibus recusandae natus possimus est libero nihil autem sit unde modi consequatur, voluptas dicta eius incidunt corporis earum, tenetur nam facilis reprehenderit suscipit. Saepe veritatis corrupti autem? Nesciunt, amet!\r\nFuga aliquid ullam ipsum quasi, facilis doloremque recusandae. Accusamus porro autem architecto, voluptates excepturi explicabo voluptatibus repellendus error distinctio qui, necessitatibus at? Hic et velit necessitatibus doloremque sapiente facilis qui.\r\nNulla quas vitae dignissimos nobis unde magni vero possimus voluptatum nostrum tempora! Quasi ad ipsam excepturi dolorum dignissimos labore, laboriosam laudantium voluptatem ipsum sequi accusantium quod officiis quia accusamus adipisci.\r\nMaiores cum ad quam porro animi culpa quis eligendi, distinctio ipsum inventore, ipsa error dicta asperiores unde corrupti beatae veniam harum cupiditate impedit illum at odit corporis. Optio, cupiditate doloremque?\r\nIpsam praesentium odit, voluptatum, beatae dicta assumenda nemo vel maxime ex omnis obcaecati! Illum illo sint soluta rem voluptatem dicta praesentium, quod itaque consectetur in tempore delectus nulla eos animi.', '2023-05-11 09:29:14', NULL, 1);
+INSERT INTO `product` (`id`, `product_code`, `category_id`, `product_name`, `price`, `thumbnail`, `description`, `created_at`, `updated_at`, `status`, `sold`, `visible`) VALUES
+(10001, 'MT001', 1005, 'Bibimbap Bò', 55000, '1684180416_BBB.jpg', 'Bibimbap bò là món cơm trộn của người Hàn Quốc với những nguyên liệu cơ bản như thịt bò xào mềm ngọt, trứng chiên lòng đào hấp dẫn, phần rau củ chín tới, tất cả hòa quyện tạo nên hương vị cực ngon miệng và hấp dẫn cho món ăn.\r\nMón ăn có sự hài hòa về màu sắc món ăn, cân bằng giữa rau và thịt sẽ giúp cho tiêu hóa và duy trì cân bằng cơ thể.\r\n\r\nMột trong những lí do mà người ta ăn bibimbap một lần là lại muốn ăn nữa, ngoài bởi vì hương vị đó còn là vì đây là món ăn có lợi cho sức khỏe. Với rất nhiều rau củ được trộn lẫn mang đến nguồn dinh dưỡng đầy đủ, không chỉ vậy món ăn chỉ sử dụng những nguyên liệu tự nhiên chứ không phải những nguyên liệu gia công nên rất tốt cho sức khỏe.\r\n', '2023-05-15 21:53:36', NULL, 1, 0, 1),
+(10002, 'MT002', 1007, 'Thập cẩm', 99000, '1684180516_AVTC.jpg', 'Chiên sản phẩm trong dầu đến khi giòn và có màu vàng ươm là bạn đã có thể thoải mái thưởng thức. Nhìn qua bên ngoài đã thấy vô cùng bắt mắt với từng cuốn bánh nhỏ xinh được bao bọc bằng mì Kataifi - Một loại mì mới lần đầu có mặt tại Việt Nam. Khi ăn miếng đầu tiên, bạn sẽ cảm nhận được ngay phần nhân chả cá dai thơm, tôm tươi hòa quyện với vị ngọt của các loại rau củ như bắp cải, cà rốt, hành lá, tất cả sẽ mang lại sự hài hòa tuyệt vời khi ăn.\r\nNgoài việc là một món ăn ngon, Tam tơ hải sản còn mang ý nghĩa là sự gắn bó, đoàn kết. Với bên trong là nhân cá, tôm, rau củ mang ý nghĩa gắn kết từ đất liền đến biển cả. Bên ngoài là lớp áo bằng sợi mì như những sợi tơ vàng óng ánh dệt nên những thành công lớn. ...\r\n', '2023-05-15 21:55:16', NULL, 1, 0, 1),
+(10003, 'MT003', 1001, 'Mì kim chi bò', 34000, '1684180593_MKCB.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...\r\n', '2023-05-15 21:56:33', NULL, 0, 0, 1),
+(10004, 'MT004', 1004, 'Lẩu thái hải sản', 210000, '1684180664_LTHSL.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 21:57:44', NULL, 1, 0, 1),
+(10005, 'MT005', 1006, 'Nước ép táo', 55000, '1684180726_NETAO.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 21:58:46', NULL, 1, 0, 1),
+(10006, 'MT006', 1007, 'Tokbokki hải sản', 55000, '1684180800_TBHS.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 22:00:00', NULL, 1, 0, 1),
+(10007, 'MT007', 1001, 'Mì kim chi bò mỹ', 49000, '1684180859_MKCBM.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 22:00:59', NULL, 1, 0, 1),
+(10008, 'MT008', 1001, 'Mì kim chi cá', 45000, '1684180906_MKCC.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 22:01:46', NULL, 1, 0, 1),
+(10009, 'MT009', 1008, 'Bắp cải xanh thêm', 9000, '1684181047_BCXtm.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé!', '2023-05-15 22:04:07', NULL, 1, 0, 1),
+(10010, 'MT0010', 1001, 'Mì kim chi thập cẩm', 54000, '1684181112_MKCTCAM.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi.\r\nĐa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...\r\n', '2023-05-15 22:05:12', NULL, 1, 0, 1),
+(10011, 'MT0011', 1008, 'Tôm thêm', 7000, '1684181182_TOMTM.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi. Đa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 22:06:22', NULL, 0, 0, 1),
+(10012, 'MT0012', 1002, 'Mì lẩu thái bò', 34000, '1684181228_MLTB.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi. Đa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 22:07:08', NULL, 1, 0, 1),
+(10013, 'MT0013', 1002, 'Mì lẩu thái gà', 30000, '1684181307_MLTG.jpg', 'Những buổi sáng trời trở lạnh, bạn có thể chủ động thay thế các món ăn sáng quen thuộc bằng những tô mì thịt bò kim chi nóng hổi và ngon xuất sắc này. Các bạn cũng biết rồi đấy, có hai thứ không thể thiếu trên bàn ăn của người Hàn đó là cơm nóng và kim chi. Đa số các món ăn trong văn hóa ẩm thực của Hàn Quốc đều có kim chi, và đây cũng là lí do mà người ta hay gọi Hàn Quốc là xứ sở kim chi hay đất nước kim chi đó các bạn. Còn Việt Nam mình thì có món nào đặc trưng nè, đố các bạn đấy và hãy comment trong phần đánh giá nhé! ...', '2023-05-15 22:08:27', NULL, 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -207,8 +215,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `avatar`, `fullname`, `email`, `phone`, `address`, `city`, `username`, `password`, `cart`, `is_block`) VALUES
-(4, 'avatar.jpg', NULL, 'hoankl222@gmail.com', '0354986796', '', 0, '3121411075', '481a4883778afa7ec63b70f163f737e6', '[{\"tensanpham\":\"abn\",\"id\":\"57\",\"soluong\":\"10\",\"giasp\":\"23000\",\"hinhanh\":\"1683365975_BB.jpg\",\"masp\":\"MT1010\"}]', 1),
-(5, 'avatar.jpg', 'Lê Văn Hoàn', 'hoanzin135@gmail.com', '0354986796', '', 0, 'hoankl456', 'ecb97ffafc1798cd2f67fcbc37226761', '[{\"tensanpham\":\"daqq\",\"id\":\"58\",\"soluong\":\"2\",\"giasp\":\"565000\",\"hinhanh\":\"1683381541_Nuocgao.jpg\",\"masp\":\"MT1010\"},{\"tensanpham\":\"abn\",\"id\":\"57\",\"soluong\":\"1\",\"giasp\":\"23000\",\"hinhanh\":\"1683365975_BB.jpg\",\"masp\":\"MT1010\"}]', 1);
+(5, 'avatar.jpg', 'le van hoan', 'hoankl222@gmail.com', '0354986796', 'duong bui cong trung', 0, 'conangkhotinh', 'ecb97ffafc1798cd2f67fcbc37226761', '[{\"tensanpham\":\"daqq\",\"id\":\"58\",\"soluong\":\"3\",\"giasp\":\"565000\",\"hinhanh\":\"1683381541_Nuocgao.jpg\",\"masp\":\"MT1010\"},{\"tensanpham\":\"abn\",\"id\":\"57\",\"soluong\":\"1\",\"giasp\":\"23000\",\"hinhanh\":\"1683365975_BB.jpg\",\"masp\":\"MT1010\"}]', 1),
+(6, 'avatar.jpg', 'abc', 'hoanzin135@gmail.com', '0354986796', 'duong bui cong trung', 0, 'hoankl456', 'dde6e7ee2694d45cb0af5bd76e52ef0c', '[{\"tensanpham\":\"Bibimbap Bu00f2\",\"id\":\"10001\",\"soluong\":\"2\",\"giasp\":\"55000\",\"hinhanh\":\"1684180416_BBB.jpg\",\"masp\":\"MT001\"},{\"tensanpham\":\"Lu1ea9u thu00e1i hu1ea3i su1ea3n\",\"id\":\"10004\",\"soluong\":\"1\",\"giasp\":\"210000\",\"hinhanh\":\"1684180664_LTHSL.jpg\",\"masp\":\"MT004\"}]', 1),
+(9, 'avatar.jpg', 'ab', 'hoanzin135@gmail.com', '0354986796', 'duong bui cong trung', 0, 'conangkhotinh', 'dde6e7ee2694d45cb0af5bd76e52ef0c', NULL, 1);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -275,37 +284,37 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT cho bảng `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1018;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1019;
 
 --
 -- AUTO_INCREMENT cho bảng `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `orders_details`
 --
 ALTER TABLE `orders_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10014;
 
 --
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
